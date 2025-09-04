6cadebc038585a6fa40a07d687c12ba1fd40b8e1
Rob Hall <123633336+rhcompbuilds@users.noreply.github.com>
2025-09-04 15:34:41 +0000

    readying code for deployment

--- swimming/swimming/settings.py
+++ swimming/swimming/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4zl$wug8h(b-oynnixt!jsdqlyi6)n=$a8-i-q2^f5i(pjulb+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ 'localhost', '.codeanywhere.com', '.herokuapp.com']
 
