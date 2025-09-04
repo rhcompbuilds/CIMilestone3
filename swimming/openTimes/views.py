935495fd1a429c060fa826ebb7cb017fa8fdff81
Rob Hall <123633336+rhcompbuilds@users.noreply.github.com>
2025-09-04 17:30:09 +0100

    Delete swimming directory

--- swimming/openTimes/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def opening_times(request):
-    return HttpResponse("This is the opening times and days view")
-    
