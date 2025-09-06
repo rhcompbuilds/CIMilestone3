3ad58a2692c5e166518546d5efbb3c08d71f8941
Rob Hall <rhcompbuilds@gmail.com>
2025-09-06 15:23:36 +0100

    create initial page

---
+++ ph_swimming_app/open_hours/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def opening_hours(request):
+    return HttpResponse("Welcome to the Swimming Pool Opening Hours page!")
