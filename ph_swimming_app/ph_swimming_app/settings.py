c0619d0535d22c9d6ddd3911b60e6ed0d8b93ce2
Rob Hall <rhcompbuilds@gmail.com>
2025-09-06 15:28:27 +0100

    setting up deploy

--- ph_swimming_app/ph_swimming_app/settings.py
+++ ph_swimming_app/ph_swimming_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o_mtd=j0de0mdw0a7+n8870a7@0zx)ak-(wqg11in@g$8#m@09'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
