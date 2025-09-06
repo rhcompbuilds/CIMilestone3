61feb8c43b24482a7df9c8514ab6d125e33ad2f0
Rob Hall <rhcompbuilds@gmail.com>
2025-09-06 16:02:08 +0100

    add opening times to admin panel

--- ph_swimming_app/open_hours/models.py
+++ ph_swimming_app/open_hours/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class OpeningHours(models.Model):
+    day = models.CharField(max_length=10)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.day}: {self.open_time} - {self.close_time})"
