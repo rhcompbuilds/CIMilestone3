983808408a364a3d03f7391bb3b08f0066688ec9
Rob Hall <123633336+rhcompbuilds@users.noreply.github.com>
2025-09-07 18:01:36 +0100

    Delete ph_swimming_app/env.py

--- ph_swimming_app/env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_tdcNKzO70nEr@ep-little-glade-a29pxlj5.eu-central-1.aws.neon.tech/upon_icy_barge_959037")
-os.environ.setdefault("SECRET_KEY", "Q%.W-=.HCEP]VQ3")
